88cb203ee55c64853ef9d0adb2147a320f3e005b
Daniil <dmtrvd90@gmail.com>
2022-09-26 23:52:07 +0300

    mobile

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const burger = document.querySelector("#burger");
-console.log(burger);
+let counter = 0;
+
+burger.onclick = function () {
+  const menuList = document.querySelector(".header__nav-left");
+  const background = document.querySelector(".header__nav-wrapper-mob");
+  if (counter === 0) {
+    menuList.style.display = "block";
+    background.style.background = "#fff";
+    counter++;
+  } else {
+    menuList.style.display = "none";
+    background.style.background = "none";
+    counter = 0;
+  }
+};
